f6dab172b04b0c2563f562caf66f2d9d3a4545ec
gg-mg <ggichuki2017@gmail.com>
2019-12-14 18:00:17 +0300

    Separated the Meats and Veggies on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,58 @@
-
 // Business Logic
-var meat = { name: "Meat", toppings: [chicken, pepperoni, crispybacon, sausage, steak] };
-var veggie = { name: "Veggie", toppings: [mixedpeppers, mushrooms, tomato, pineapple] };
-var toppings=[meat,veggie]
+// var meat = {
+//     name: "Meat",
+//     toppings: [chicken, pepperoni, crispybacon, sausage, steak]
+// };
+// var veggie = {
+//     name: "Veggie",
+//     toppings: [mixedpeppers, mushrooms, tomato, pineapple]
+// };
 
-function Pizza(pizzaSize, crust,toppings) {
+
+function Pizza(pizzaSize, crust, toppings) {
     this.pizzaSize = pizzaSize;
     this.crust = crust;
-    this.toppings = toppings;
-    
+    this.meatToppings = [];
+    this.vegToppings = [];
+}
+
+Pizza.prototype.addMeat = function(meat) {
+    this.meatToppings.push(meat);
   }
-  Pizza.prototype.cost = function() {
+  Pizza.prototype.addVeg = function(veggie) {
+    this.vegToppings.push(veggie);
+  }
+Pizza.prototype.calculateCost = function () {
     var cost = 0;
-    
+
     if (this.pizzaSize === "small") {
-      cost = 500;
+        cost = 500;
     }
-      if (this.pizzaSize === "medium") {
+    if (this.pizzaSize === "medium") {
         cost = 700;
-    } if (this.pizzaSize === "large") {
-      cost = 900;
-    } 
-  
-  if (this.crust === "Crispy") {
-    cost += 200;
-  }
+    }
+    if (this.pizzaSize === "large") {
+        cost = 900;
+    }
+
+    if (this.crust === "Crispy") {
+        cost += 200;
+    }
     if (this.crust === "Italian") {
-      cost = 150;
-  } if (this.crust === "Stuffed_Crust") {
-    cost = 100;
-  } if (this.crust === "Gluten_Free") {
-    cost = 250;
-  } 
-  this.toppings.forEach(function() {
-    cost += 200;
-  });
-  this.toppings.forEach(function() {
-    cost += 150;
-  });
-  this.cost = cost;
+        cost = 150;
+    }
+    if (this.crust === "Stuffed_Crust") {
+        cost = 100;
+    }
+    if (this.crust === "Gluten_Free") {
+        cost = 250;
+    }
+    this.meat.forEach(function () {
+        cost += 200;
+    });
+    this.veggie.forEach(function () {
+        cost += 150;
+    });
+    this.cost = cost;
 
 }
